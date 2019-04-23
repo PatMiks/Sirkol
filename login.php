97587d658808a9313c6ab31df4a478a19e11a30a
PatMiks <34721148+PatMiks@users.noreply.github.com>
2019-04-23 21:40:23 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,23 +1,3 @@
 <?php
-
-    include 'SteamLogin.php';
-    
-$url = SteamLogin::genUrl('http://mywebsite.com/login/response');
-
-
-  <a href="<?php echo $url; ?>">Login with Steam</a>
-
-$response = SteamLogin::validate();
-
-if( empty( $response ) ) {
-  
-  return 'The Steam Login request has now expired, please try again.';
-  
-}
-else {
-  
-  return $response; // Community ID
-  
-}
-
+echo "test"
 ?>
